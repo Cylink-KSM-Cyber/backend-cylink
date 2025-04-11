c86124237a31a67a2b09e6c9467e9f9191a54ad3
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-04-11 20:32:02 +0700

    feat: enhance response utility to support pagination in API responses

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,19 +1,40 @@
-import { Response } from 'express';
+import { Response } from "express";
 
+/**
+ * Response Utility
+ *
+ * Provides standardized response formatting for API endpoints
+ * @module utils/response
+ */
+
+/**
+ * Sends a standardized API response
+ *
+ * @param {Response} res - Express response object
+ * @param {number} statusCode - HTTP status code
+ * @param {string} message - Response message
+ * @param {any} data - Response data (can be any type)
+ * @returns {Response} Express response with formatted JSON
+ */
 exports.sendResponse = (
   res: Response,
   statusCode: number,
   message: string,
   data = null,
+  pagination = null
 ) => {
   const layout: any = {
     status: statusCode,
     message,
   };
 
-  if (data) {
+  if (data !== null) {
     layout.data = data;
   }
 
+  if (pagination !== null) {
+    layout.pagination = pagination;
+  }
+
   return res.status(statusCode).json(layout);
-}
+};
